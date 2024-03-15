3db831c4d56cb5c4000eebca63408cdb5c1cc797
cesse <cesse@172.17.118.176>
2024-03-15 20:40:57 +0100

    classe piece ajoutée

---
+++ src/main/java/fr/insa/porjet/leporjet/Coin.java
@@ -0,0 +1,54 @@
+package fr.insa.porjet.leporjet;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Elève
+ */
+public class Coin {
+    private int id;
+    private double x;
+    private double y;
+
+    public Coin(int id, double x, double y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Coin{" + "id=" + id + ", x=" + x + ", y=" + y + '}';
+    }
+    
+    
+    
+}
