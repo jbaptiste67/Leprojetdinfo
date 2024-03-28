8773a4d28d6fd10c9c0c08c2895bef86901676d8
ccesselin01 <ccesselin01@PC-0298.csip.in>
2024-03-28 09:50:30 +0100

    Merge origin/master

--- src/main/java/fr/insa/porjet/leporjet/Leporjet.java
+++ src/main/java/fr/insa/porjet/leporjet/Leporjet.java
@@ -18,5 +18,21 @@ public class Leporjet {
         System.out.println(c1.toString());
         System.out.println(c2.toString());
         System.out.println( "surface : "+ m1.surface());
+        
+        /* demande de budget
+        immeuble ou maison
+longueur largeur et hauteur du bat (calcul surface facade, revêtement pour la façade?)
+nb d'étages et hauteur de chaque niveau
+nombre de pièces par étage
+fonction et coordonnées de chaque pièce, choix des revêtements pour chaque élément de la pièce
+nb de fenêtres et d'ouvertures etc
+calcul du devis : budget respecté ?
+        */
+        System.out.println("Quel est votre budget pour ce devis?");
+        Lire.i()= int B ;
+        System.out.println("Votre bâtiment est-il un immeuble ou une maison");
+        Lire.b()=boolean type ;
+        
+        
     }
 }
