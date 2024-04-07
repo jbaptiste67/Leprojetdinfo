4b51a79fe78bb9d780557927fbd6258cc4228746
cesse <cesse@172.17.118.176>
2024-04-07 18:26:56 +0200

    Merge origin/master
    Conflicts:
    	src/main/java/fr/insa/porjet/leporjet/Etage.java

--- src/main/java/fr/insa/porjet/leporjet/Batiment.java
+++ src/main/java/fr/insa/porjet/leporjet/Batiment.java
@@ -8,7 +8,25 @@ package fr.insa.porjet.leporjet;
  *
  * @author Elève
  */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Batiment {
-    private H; //hauteur totale du bâtiment
+    private List<Etage> etages;
     
+    public Batiment() {
+        etages = new ArrayList<>();
+    }
+
+    public void ajouterEtage(Etage etage) {
+        etages.add(etage);
+    }
+
+    public List<Etage> getEtages() {
+        return etages;
+        
+    }
+    //private H; //hauteur totale du bâtiment
+    // demander le nombre d'étages
+    // demander si étages de même hauteur, selon la réponse renvoyer à des procédures/fonctions de la classe étage
 }
