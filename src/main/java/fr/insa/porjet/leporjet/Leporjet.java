c456d65a268f4395961ff425e8cff8c2bc15d19d
cesse <cesse@172.17.118.176>
2024-04-07 18:27:25 +0200

    Merge origin/master
    Conflicts:
    	src/main/java/fr/insa/porjet/leporjet/Etage.java

--- src/main/java/fr/insa/porjet/leporjet/Leporjet.java
+++ src/main/java/fr/insa/porjet/leporjet/Leporjet.java
@@ -8,16 +8,20 @@ package fr.insa.porjet.leporjet;
  *
  * @author Elève
  */
+import fr.insa.porjet.leporjet.*;
+
+
 public class Leporjet {
 
     public static void main(String[] args) {
-        Coin c1 = new Coin(1,3.15,4.5);
+        /*Coin c1 = new Coin(1,3.15,4.5);
         Coin c2 = new Coin(2,7,9);
         Mur m1= new Mur(1,c1,c2); 
         
         System.out.println(c1.toString());
         System.out.println(c2.toString());
         System.out.println( "surface : "+ m1.surface());
+        */
         
         /* demande de budget
         immeuble ou maison
@@ -31,27 +35,65 @@ calcul du devis : budget respecté ?
         System.out.println("Quel est votre budget pour ce devis?");
         int B = Lire.i()  ;
 
-        
+        // Instanciation batiment
+Batiment batiment = new Batiment();
+    
 System.out.println("Quelle est la hauteur de votre bâtiment ?");
-double h=Lire.d();
+double hauteur=Lire.d();
 
 System.out.println("Combien d'étages a votre bâtiment ?"); 
        int nbetage=Lire.i();
+      
        
-System.out.println("tous vos étages font-ils la même hauteur ?");
-String reponse=Lire.S();
-while (reponse!="oui"&&"non"){
-    System.out.println("tous vos étages font-ils la même hauteur ?");
-var reponse=Lire.S();
-if ("oui".equals(reponse)) {
-    double hauteur=h/nbetage;
-}if ("non".equals(reponse)){
-        System.out.println("donnez la hauteur de chaque etage en partant du rdc");
-        for (int i=1; i<=nbetage; i++) {
-            //comment différencier chaque lire étage? new etage ??//
-        }}}
-//A REVOIR// 
-System.out.println("Combien de pièces y a-t-il dans l'étage ?");
+for (int i =1; i<=nbetage ; i++) {
+    
+    Etage etage = new Etage (); // dans la boucle les objets vont s'appeler pareil???
+    System.out.println("Combien de pièces y a-t-il à l'étage "+i+" ?");
+    int nbpiece = Lire.i();
+    
+    for (int k =1; k<=nbpiece ; k++) {
+        // instanciation piece
+        Piece piece = new Piece ();
+         // Ajout des pièces à l'étage
+        etage.ajouterPiece(piece);
+        // demander rôle de la pièce : à voir plus tard
+        
+        for (int m =1; m<=4 ; m++) { // probleme : tourne a l'infini
+            
+            // demande les info sur les coins début et fin
+            System.out.println("Donner les coordonnées du coin début du mur"+m);
+            double a = Lire.d();
+            double b = Lire.d();
+            System.out.println("Donner l'identifiant du coin");
+            int id1 = Lire.i();
+            
+            System.out.println("Donner les coordonnées du coin fin du mur"+m);
+            double c = Lire.d();
+            double d = Lire.d();
+            System.out.println("Donner l'identifiant du coin");
+            int id2 = Lire.i();
+            
+            // création des objets coins
+            Coin cd = new Coin (id1, a, b);
+            Coin cf = new Coin (id2, c, d);
+            
+            // création d'un mur avec les coins cd et cf
+            Mur mur = new Mur(m, cd, cf);
+    
+            piece.ajouterMur(mur); // y a aura surement des pb d'identifiants, plusieurs murs risquent de s'appeler 1 ?
+            
+            //calcul de surface
+            double Surface = mur.surface(hauteur);
+            
+            // demander le revêtement
+            // calculer le prix du revêtement (en fonction de la surface du mur)
+        }
+    }
+    // Ajout de l'étage au bâtiment
+    batiment.ajouterEtage(etage);
+}
+        
+
 
         }
     
